8e8c42bd8444ada1530fc1c2fc22b9315c600f12
yubo <wuyubo0522@163.com>
2019-12-12 11:24:52 +0800

    更新build.gradle文件配置，再次发布依赖测试

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # OpenAT
+
+### 使用方式
+```
+	allprojects {
+		repositories {
+			...
+			maven { url 'https://jitpack.io' }
+		}
+	}
+```
+```
+    dependencies {
+	    implementation 'com.github.wuyubo0522:OpenAT:1.0.0'
+	}
+```
---
+++ app/src/main/java/com/yb/test/MainActivity.java
@@ -0,0 +1,18 @@
+package com.yb.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * 类说明：OpenAT测试工具包测试界面
+ * @author 裕博
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
